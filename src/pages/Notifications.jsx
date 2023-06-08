002d181a2e4814f501315429099cf89c1c66133d
my-amtech <ammuftau74@gmail.com>
2023-06-08 23:42:48 +0100

    calender commit

--- src/pages/Notifications.jsx
+++ src/pages/Notifications.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Notifications = () => {
 	return (
 		<div className="mx-2 pt-10 h-screen relative bg-green-50">
-			<h3 className="text-center text-xl font-semibold m-4 mt-10">
+			<h3 className="text-center text-xl md:text-2xl font-semibold m-4 mt-10 z-20 relative w-8/12 mx-auto text-green-500">
 				Notifications
 			</h3>
 			<div className="space-y-2 px-px md:px-2 text-sm">
@@ -15,6 +15,36 @@ const Notifications = () => {
 					<div className="flex justify-between items-center">
 						<p className="font-semibold">Send to Musa Mik...</p>
 						<p className="font-semibold">08:06, Jan 03</p>
+					</div>
+				</div>
+				<div className="bg-white rounded-lg px-4 py-4">
+					<div className="flex justify-between items-center">
+						<p className="font-semibold text-green-500 p-1">+1,200.00</p>
+						<p className="font-semibold text-green-500">Successful</p>
+					</div>
+					<div className="flex justify-between items-center">
+						<p className="font-semibold">Receive from John Abr...</p>
+						<p className="font-semibold">08:06, Dec 17</p>
+					</div>
+				</div>
+				<div className="bg-white rounded-lg px-4 py-4">
+					<div className="flex justify-between items-center">
+						<p className="font-semibold text-green-500 p-1">+1,200.00</p>
+						<p className="font-semibold text-green-500">Successful</p>
+					</div>
+					<div className="flex justify-between items-center">
+						<p className="font-semibold">Receive from John Abr...</p>
+						<p className="font-semibold">08:06, Dec 17</p>
+					</div>
+				</div>
+				<div className="bg-white rounded-lg px-4 py-4">
+					<div className="flex justify-between items-center">
+						<p className="font-semibold text-green-500 p-1">+1,200.00</p>
+						<p className="font-semibold text-green-500">Successful</p>
+					</div>
+					<div className="flex justify-between items-center">
+						<p className="font-semibold">Receive from John Abr...</p>
+						<p className="font-semibold">08:06, Dec 17</p>
 					</div>
 				</div>
 				<div className="bg-white rounded-lg px-4 py-4">
